68b1259003fb0e021968ffeda5fad128345b5621
Mykolas Meidus <mykolas.meidus@gmail.com>
2026-08-29 20:38:40 +0300

    killfeed

---
+++ KillFeed.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class KillFeed : VBoxContainer
+{
+	[Export] public float MessageDuration = 4.0f;
+
+	public static KillFeed Instance { get; private set; }
+
+	public override void _EnterTree()
+	{
+		Instance = this;
+	}
+
+	public void AddLog(string attackerName, string victimName)
+	{
+		Label logLabel = new Label
+		{
+			Text = $"{attackerName} ➔ {victimName}",
+			HorizontalAlignment = HorizontalAlignment.Right
+		};
+
+		// Styling for dark background visibility
+		logLabel.AddThemeColorOverride("font_color", new Color(1f, 0.3f, 0.3f));
+		logLabel.AddThemeFontSizeOverride("font_size", 14);
+
+		AddChild(logLabel);
+
+		// Despawn log line after duration
+		GetTree().CreateTimer(MessageDuration).Timeout += () => logLabel.QueueFree();
+	}
+}
